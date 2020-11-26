c98a3867b2e1ef59a4b2336c07c850b3a8addf4a
SudhanshuSuman <sudhanshusuman.05@gmail.com>
2020-11-26 18:04:00 +0530

    'checked' radio/checkbox issue fix

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ var array = [];
 $('button').on('click', function() {
     // var array = [];
     $("input:checked").each(function() {
-        array.push($(this).val());
+        if($(this).val() != "") {
+            array.push($(this).val());
+        }
     });
     $('p').text(array);
     console.log(array)
@@ -12,15 +14,30 @@ $('button').on('click', function() {
     });
 });
 
-$('input[class="predictor"]').on('change', function(){
-    if ($(this).attr('type') == 'radio' ) {
-        if ( $(this).prop('checked') ) {
-            $('input[class="predictor"][type="checkbox"]').prop('checked', false);
+
+var q = ["gender", "c-sympt", "m-sympt", "s-sympt", "other-prob"]
+function checkedchkr (name, obj) {
+    console.log(name, $(obj).attr('type'))
+    if ($(obj).attr('type') == 'radio' ) {
+        if ( $("input[name=" + name + "]").filter(function() { return this.prop == 'checked'; }) ) {
+            $("input[name=" + name + "][type='checkbox']").prop('checked', false);
+        }
+    } else {
+        if ( $("input[name=" + name + "]").filter(function() { return this.prop == 'checked'; }) ) {
+            $("input[name=" + name + "][type='radio']").prop('checked', false);
         }
     }
-    else {
-        if ( $(this).prop('checked') ) {
-            $('input[class="predictor"][type="radio"]').prop('checked', false);
-        }
-    }
-});
+}
+$("input[name=" + q[0] + "]").on('change', function () { checkedchkr(q[0], $(this)); });
+$("input[name=" + q[1] + "]").on('change', function () { checkedchkr(q[1], $(this)); });
+$("input[name=" + q[2] + "]").on('change', function () { checkedchkr(q[2], $(this)); });
+$("input[name=" + q[3] + "]").on('change', function () { checkedchkr(q[3], $(this)); });
+$("input[name=" + q[4] + "]").on('change', function () { checkedchkr(q[4], $(this)); });
+
+
+// for Covid-19:
+var csympt = ["Fever", "Dry Cough", "Fatigue"];
+var msympt = ["Loss of taste or smell", "Nasal Congestion", "Conjunctivitis", "Sore Throat", "Headache", "Muscle or joint pain", "Nausea/Vomiting", "Diarrhea", "Dizziness"];
+var ssympt = ["Shortness of breath", "Loss of appetite", "Persistent pain in the chest", "High body temperature", "Strokes", "Nerve Damage"];
+var otherprob = ["Asthama", "Diabeties", "Blood Pressure", "Heart Disease", "Kidney Disorder", "Lung Disease", "Hypertension"];
+
