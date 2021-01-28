187d4c9ff2294f9d04a03d1452c4b5404c587366
Sudhanshu Suman <sudhanshusuman.05@gmail.com>
2021-01-28 22:39:59 +0530

    Updated all

--- js/ajaxUtils.js
+++ js/ajaxUtils.js
@@ -1,8 +1,6 @@
 (function (global) {
-
     // Set up a namespace for our utility
     var ajaxUtils = {};
-    
     
     // Returns an HTTP request object
     function getRequestObject() {
@@ -19,36 +17,26 @@
       }
     }
     
-    
     // Makes an Ajax GET request to 'requestUrl'
     ajaxUtils.sendGetRequest = 
       function(requestUrl, responseHandler, isJsonResponse) {
         var request = getRequestObject();
         request.onreadystatechange = 
           function() { 
-            handleResponse(request, 
-                           responseHandler,
-                           isJsonResponse); 
+            handleResponse(request, responseHandler, isJsonResponse); 
           };
         request.open("GET", requestUrl, true);
         request.send(null); // for POST only
       };
-    
-    
     // Only calls user provided 'responseHandler'
     // function if response is ready
     // and not an error
-    function handleResponse(request,
-                            responseHandler,
-                            isJsonResponse) {
-      if ((request.readyState == 4) &&
-         (request.status == 200)) {
-    
+    function handleResponse(request, responseHandler,isJsonResponse) {
+      if ((request.readyState == 4) && (request.status == 200)) {
         // Default to isJsonResponse = true
         if (isJsonResponse == undefined) {
           isJsonResponse = true;
         }
-    
         if (isJsonResponse) {
           responseHandler(JSON.parse(request.responseText));
         }
@@ -57,8 +45,6 @@
         }
       }
     }
-
     // Expose utility to the global object
     global.$ajaxUtils = ajaxUtils;
-
 })(window);
